435b6681743e3b5e00cebaad5c8da2c7074db788
Malcom Fetus <sora.vista@gmail.com>
2020-04-04 22:11:26 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,1 +1,8 @@
-groceries.keys
+groceries = 
+  {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+  }
+end
